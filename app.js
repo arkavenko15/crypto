779c9e39a0679ac5c9a2ee7dcfd9d3f7e74c0759
Liuda Arkavenko <arkavenko18@gmail.com>
2021-05-16 14:09:01 +0300

    added theme changer

--- app.js
+++ app.js
@@ -13,3 +13,48 @@ $(document).ready(function(){
         mar.data('interval',setInterval(mar.marquee,1000/140));
     });
     });
+
+$(function() {
+ 
+        $(window).scroll(function() {
+         
+        if($(this).scrollTop() != 0) {
+         
+        $('.myBtn').fadeIn();
+         
+        } else {
+         
+        $('.myBtn').fadeOut();
+         
+        }
+         
+        });
+         
+        $('.myBtn').click(function() {
+         
+        $('body,html').animate({scrollTop:0},2000);
+         
+        });
+         
+}); 
+
+
+let switchButton = document.querySelector(".switch-btn");
+let sliderRoundLeft = document.querySelector(".slider-round-left");
+let body = document.querySelector("main")
+console.log(body)
+switchButton.addEventListener('click',function(){
+    if(sliderRoundLeft.classList.contains("slider-round-left"))
+    {
+        sliderRoundLeft.classList.remove("slider-round-left")
+        sliderRoundLeft.classList.add("slider-round-right")
+        body.setAttribute("style", "background-color:silver; ");
+        console.log("hello")
+    }
+    else
+    {
+        sliderRoundLeft.classList.add("slider-round-left");
+        sliderRoundLeft.classList.remove("slider-round-right");
+        body.setAttribute("style", "background-color:#4b4b4b; ");
+    }
+})
